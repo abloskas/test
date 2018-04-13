c5d39106c73edb49b6a1e0e81bff8b7154db1423
Ashley <a.bloskas@yahoo.com>
2018-04-12 18:52:20 -0700

    test

---
+++ spec/strings_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Strings do
+  it "has a version number" do
+    expect(Strings::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
